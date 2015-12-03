055193c167e65a505e76b3c963b4df70b6fa9a8d
Nasos Rossikopoulos <nassos@arossiko@lab.grid.auth.gr>
2015-12-03 16:14:02 +0200

    ra-org relations corrections

--- access/db/migrate/20151124180329_create_ra_organization_relations.rb
+++ access/db/migrate/20151124180329_create_ra_organization_relations.rb
@@ -1,8 +1,8 @@
 class CreateRaOrganizationRelations < ActiveRecord::Migration
   def change
     create_table :ra_organization_relations do |t|
-      t.integer :ra_id
-      t.integer :organization_id
+      t.belongs_to :registration_authority, foreign_key: :ra_id
+      t.belongs_to :organization, foreign_key: :organization_id
       t.timestamps null: false
     end
   end
